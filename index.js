348fb1aa5f9fd445cb4a3b32f617bc78e9220941
Sayan Riju Chakrabarti <s26c.sayan@gmail.com>
2018-08-08 11:05:48 +0530

    CORS support

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ const exampleNos = [
  */
 module.exports = async (req, res) => {
   // console.log(req);
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Request-Method", "*")
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT")
+  res.setHeader("Access-Control-Allow-Headers", "*")
+  if (req.method === "OPTIONS") {
+    res.writeHead(200)
+    res.end()
+    return
+  }
+
   try {
     const { action, mobile, otpReceived } = await json(req)
     if (action === undefined) throw createError(400, "Field `action` is mandatory!")
